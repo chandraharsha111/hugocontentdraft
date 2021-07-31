afc57366e615664c98537c49d6c0f62e9867fedf
ChandraHarsha <chandraharsha111@gmail.com>
2021-07-31 16:59:12 -0500

    Update shell

--- deploy.sh
+++ deploy.sh
@@ -9,7 +9,7 @@ printf "\033[0;32mDeploying updates to GitHub...\033[0m\n"
 hugo # if using a theme, replace with `hugo -t <YOURTHEME>`
 hugo -d ../chandraharsha111.github.io
 
-# Deploy your public site
+# Try deploying to public site
 cd ../chandraharsha111.github.io # Goes to your public site repo
 git add . # Adds changes
 
@@ -21,7 +21,7 @@ if [ -n "$*" ]; then
 fi
 git commit -m "$msg"
 
-# Push source and build repos.
+# Push source and build repos to deployed site on Github.
 git push origin main
 printf "\033[0;32m Deployed the website ...\033[0m\n"
 
@@ -34,5 +34,5 @@ if [ -n "$*" ]; then
 	msg="$*"
 fi
 
-git commit -m "$msg" # Makes your commit to hugocontent draft repo
-git push origin main # Pushes the code to GitHub
+git commit -m "$msg" # Makes your commit to hugocontentdraft repo
+git push origin main # Pushes the code to hugocontentdraft repo
